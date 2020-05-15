3c9f2b047689d6d99153545d5acc33cd992310dc
sam gerendasy <samgerendasy@gmail.com>
2020-05-14 22:43:40 -0700

    started the  crew

---
+++ DarkMatterWeb/Models/CrewMember.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DarkMatterWeb.Models
+{
+    public class CrewMember
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Github { get; set; }
+        public string Specialities { get; set; }
+    }
+}
