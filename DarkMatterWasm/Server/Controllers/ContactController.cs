ca054734106b6419c7b7322180d58a73294734b2
Mark Davis <311063+nohorse@users.noreply.github.com>
2020-07-24 21:59:15 -0700

    broken but compiles

---
+++ DarkMatterWasm/Server/Controllers/ContactController.cs
@@ -0,0 +1,47 @@
+﻿using DarkMatterWasm.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DarkMatterWasm.Shared.Models;
+using DarkMatterWeb.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+
+namespace DarkMatterWasm.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContactController : ControllerBase
+    {
+        
+        private readonly ILogger<ContactController> logger;
+        private readonly AirTableService airtableService;
+
+        public ContactController(ILogger<ContactController> logger, AirTableService airtableService)
+        {
+            this.logger = logger;
+            this.airtableService = airtableService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Contact> Get()
+        {
+           return new List<Contact>();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Contact contact)
+        {
+            var result = await airtableService.ContactUs(contact);
+            if (result)
+            {
+                return Ok();
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ DarkMatterWasm/Shared/Models/Contact.cs
@@ -0,0 +1,12 @@
+﻿namespace DarkMatterWasm.Shared.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Message { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Company { get; set; }
+
+    }
+}
